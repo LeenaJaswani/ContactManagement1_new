2208614dd0bd6867d44a63c88571b43c4732ada1
leena <leena.jaswani@gmail.com>
2024-01-03 19:03:29 +0530

    update

--- contact_manager_backend/settings.py
+++ contact_manager_backend/settings.py
@@ -65,7 +65,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
     "http://127.0.0.1:8000",
      'http://localhost:8000',
-    'https://lj2.pythonanywhere.com' ,
+    'https://lj2.pythonanywhere.com/contacts/' ,
     'https://contactmanagement-seven.vercel.app'
     # The origin of your Angular application
 ]
